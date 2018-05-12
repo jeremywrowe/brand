550612e085232924facd516edb9c07196c030b87
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2018-05-12 13:13:36 -0400

    starting to build out resume data

---
+++ app/models/experience.rb
@@ -0,0 +1,15 @@
+class Experience
+  attr_reader :company, :title, :start_date, :end_date, :description
+
+  def initialize(company, title, start_date, end_date, description)
+    @company = company
+    @title = title
+    @start_date = start_date
+    @end_date = end_date
+    @description = description
+  end
+
+  def duration
+    "#{start_date} - #{end_date}"
+  end
+end
