124c36ac6777efe39128d72bb4ed270ef0a8b771
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2018-05-20 16:54:10 -0400

    cleanup unused folders and add a system test for contributions

---
+++ test/system/visiting_the_home_pages_test.rb
@@ -0,0 +1,9 @@
+require 'application_system_test_case'
+
+class VisitingTheHomePagesTest < ApplicationSystemTestCase
+  test 'When a user visits the home page, they can see my github contributions' do
+    visit root_url
+
+    assert_selector 'h2', text: /[\d|,]+ contributions in the last year/
+  end
+end
