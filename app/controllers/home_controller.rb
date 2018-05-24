275534f1ca71fff09b95610395210856b93808f7
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2018-05-23 21:41:38 -0400

    add pdf download button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,11 @@
 class HomeController < ApplicationController
   def index
     @resume_content = ResumeContent.new
+    pdf_resume = Pdf::Resume.new(@resume_content)
+
+    respond_to do |format|
+      format.html
+      format.pdf { send_data pdf_resume.render, disposition: :inline, filename: "#{@resume_content.avatar.name}.pdf", type: 'application/pdf' }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/resume', to: 'home#index', as: :resume
   root 'home#index'
 end
