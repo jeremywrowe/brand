efe75e9016682c4599ad322257cfb87b2283a28a
Jeremy W. Rowe <jeremywrowe@users.noreply.github.com>
2019-01-19 17:08:13 -0500

    fix failing experience test

--- test/models/experience_test.rb
+++ test/models/experience_test.rb
@@ -4,15 +4,15 @@ require 'test_helper'
 
 class ExperienceTest < ActiveSupport::TestCase
   test '#duration - defaults to "present" when no end date is provided' do
-    experience = Experience.new('', '', ['August 2020'], [], '')
-    experience_with_blank = Experience.new('', '', ['August 2020'], [' '], '')
+    experience = Experience.new('', '', ['August 2020'], [], '', '')
+    experience_with_blank = Experience.new('', '', ['August 2020'], [' '], '', '')
 
     assert_equal ['August 2020 - Present'], experience.durations
     assert_equal ['August 2020 - Present'], experience_with_blank.durations
   end
 
   test '#durations - uses both start and end date when provided' do
-    experience = Experience.new('', '', ['August 2020'], ['March 2222'], '')
+    experience = Experience.new('', '', ['August 2020'], ['March 2222'], '', '')
 
     assert_equal ['August 2020 - March 2222'], experience.durations
   end
